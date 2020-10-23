04a84c787232d6f502ba7dc968d262d829bc9578
aksdr <ingaksdr@gmail.com>
2020-10-23 14:39:39 +0300

    added quote

--- public/quote.php
+++ public/quote.php
@@ -1,2 +1,15 @@
 require("../includes/config.php");
 
+// if user reached page via GET (as by clicking a link or via redirect)
+    if ($_SERVER["REQUEST_METHOD"] == "GET")
+    {
+        // else render form
+        render("login_form.php", ["title" => "Log In"]);
+    }
+
+    // else if user reached page via POST (as by submitting a form via POST)
+    else if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+
+        
+    }
