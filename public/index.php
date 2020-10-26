b0f36baa46c806e8ee696fd74eabc715ae6c268f
aksdr <ingaksdr@gmail.com>
2020-10-26 13:41:08 +0300

    made first portfolio

--- public/index.php
+++ public/index.php
@@ -3,7 +3,18 @@
     // configuration
     require("../includes/config.php"); 
 
+    $positions = CS50::query("SELECT * FROM portfolio WHERE user_id = ?", $_SESSION["id"]);
+    
+        for ($i = 0; $i < count($positions); $i++) 
+        {
+            $price = lookup($positions[$i]["symbol"]);
+            
+            $positions[$i]["price"] = $price["price"];
+            //dump($position);
+        }
+    //dump($positions);
+
     // render portfolio
-    render("portfolio.php", ["title" => "Portfolio"]);
+    render("portfolio.php", ["title" => "Portfolio","positions"=> $positions]);
 
 ?>
