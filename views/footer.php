084fa799e27838b26d3ec4874d895d6e84c75f15
aksdr <ingaksdr@gmail.com>
2020-10-27 14:13:52 +0300

    redacting sell

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
             </div>
 
-            <div id="bottom">
+            <div id="bottom" >
                 Brought to you by the number <a href="http://cdn.cs50.net/2015/fall/psets/7/pset7/pset7.html">7</a>.
             </div>
 
--- views/sell_form.php
+++ views/sell_form.php
@@ -11,35 +11,45 @@
         </div>
         <div class="form-group">
             <button class="btn btn-default" type="submit">
-                <span aria-hidden="true" class="glyphicon glyphicon-hourglass"></span>
-                Get price
+                <span aria-hidden="true" class="glyphicon glyphicon-briefcase"></span>
+                Sell
             </button>
         </div>
     </fieldset>
 </form>
-<div>
+<div class="row">
+    <div class="col-md-4 col-md-offset-4">
     
-    <?php
-        if ($selling)
-        {
-            echo "You sold {$share} percents of {$Symbol}<b/r>" ;
-        }
-
-            echo '<table class= "table table-bordered">';
-   
-        if ($positions) 
-        {
-        foreach ($positions as $position)
+        <?php
+            if ($selling)
             {
-                print("<tr>");
-                print("<td>" . $position["symbol"] . "</td>");
-                print("<td>" . $position["share"] . "</td>");
-                print("<td>" . $position["price"] . "</td>");
-                print("</tr>");
+                echo "You sold {$share} percents of {$Symbol}<b/r>" ;
             }
-        }
+
+                echo '<table class= "table table-bordered">';
+    
+            if ($positions) 
+            {
+                echo "<tr>
+                <td> Symbol </td>
+                <td> Share,% </td>
+                <td> Price,$ </td>
+                </tr>
+                ";
+
+                foreach ($positions as $position)
+                    {
+                        print("<tr>");
+                        print("<td>" . $position["symbol"] . "</td>");
+                        print("<td>" . $position["share"] . "</td>");
+                        print("<td>" . $position["price"] . "</td>");
+                        print("</tr>");
+                    }
+            }
         
 
-    ?> 
+        ?> 
             </table>
     </div>
+</div>
+   
